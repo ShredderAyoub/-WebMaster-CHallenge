a076e4112e32757b7d136107e6287afc83a8e476
ShredderAyoub <rezzouk.ayouble@gmail.com>
2019-03-10 02:21:18 +0100

    My first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React, { Component } from 'react';
+import ReactDom from 'react-dom';
+import {Switch,Route} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-bootstrap/dist/react-bootstrap';
+import Navbar from'./components/Navbar';
+import  Shoplist from'./components/Shoplist';
+import  Details from'./components/Details';
+import  Preflist from'./components/Preflist';
+import  Default from'./components/Default';
+import Shop from './components/Shop';
+import Register from './components/reg';
+import Login from './components/log';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+     <React.Fragment>
+     <Navbar></Navbar>
+     <Switch>
+       <Route exact path='/' component={Shoplist}/>
+       <Route exact path='/details' component={Details}/>
+       <Route exact path='/Preflist' component={Preflist}/>
+       <Route exact path='/register' component={Register}/>
+       <Route exact path='/login' component={Login}/>
+       
+       <Route component={Default}/>
+     </Switch>
+     
+     
+     </React.Fragment>
     );
   }
 }
